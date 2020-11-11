e0fc1787ae3b8aa109687e533033d7173e3ea00f
Linh Nguyen <nguy4006@umn.edu>
2020-11-10 19:53:29 -0600

    added cleaning script and codebook

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# stat8051
-Group Project on 2020 Travelers Modeling Competition - Predicting Claim Cost
+# STAT 8051 - Group Project
+2020 Travelers Modeling Competition - Predicting Claim Cost: Group 4  
+
+Includes cleaning and analysis script.
---
+++ Script.R
@@ -0,0 +1,45 @@
+#################################################
+##          STAT 8051 Kaggle Project           ##
+##           Linh Nguyen - Group 4             ##
+##           Created: 10-Nov-2020              ##
+##         Last updated: 10-Nov-2020           ##
+#################################################
+
+# META ====
+# Libraries
+library(tidyverse)
+library(codebook)
+
+# Data
+data <- read.csv("InsNova_train.csv")
+
+# CLEANING ====
+# > Make sure variable types are correct ----
+names <- c("id", "veh_body", "gender", "area")
+data[,names] <- 
+  lapply(data[,names], as.factor)
+
+rm(names)
+
+# > Variable lables ----
+var_label(data$id) <- "Policy key"
+var_label(data$veh_value) <- "Market value of the vehicle in $10,000's"
+var_label(data$veh_body) <- "Type of vehicles"
+var_label(data$veh_age) <- "Age of vehicles"
+var_label(data$gender) <- "Gender of driver"
+var_label(data$area) <- "Driving area of residence"
+var_label(data$dr_age) <- "Driver's age category"
+var_label(data$exposure) <- "The basic unit of risk underlying an insurance premium"
+var_label(data$claim_ind) <- "Indicator of claim"
+var_label(data$claim_count) <- "The number of claims"
+var_label(data$claim_cost) <- "Claim amount"
+
+# > Value labels ----
+val_labels(data$veh_age) <- c("Youngest" = 1,
+                              "Oldest" = 4)
+
+val_labels(data$dr_age) <- c("Young" = 1,
+                             "Old" = 6)
+
+val_labels(data$claim_ind) <- c("No" = 0,
+                                "Yes" = 1)
